5d570512a98d8ce32df8ae84d388ea56c2f866d7
Vladyslav Hutych <mandarineye@gmail.com>
2023-04-04 20:45:42 +0200

    refactor(server): Use Nginx for serving static content, remove HTTP server

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
-import { createServer } from 'node:http';
 import { WebSocketServer } from 'ws';
 
-import { HttpRequestHandler } from './handlers/HttpRequestHandler.js';
 import { WsRequestHandler } from './handlers/WsRequestHandler.js';
 
 const port = Number(process.env.PORT);
-const httpRequestHandler = new HttpRequestHandler();
-const server = createServer(httpRequestHandler.loadFile).listen(port);
-
 const wsRequestHandler = new WsRequestHandler();
-const ws = new WebSocketServer({ server });
+
+const ws = new WebSocketServer({ port });
 ws.on('connection', wsRequestHandler.handleConnection);
